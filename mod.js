a341a4958986ecdd32aa0728a694095f2f97489e
Keith Cirkel <keithamus@users.noreply.github.com>
2023-06-23 17:12:39 +0100

    refactor to allow for unicode idents
    fixes #2
    
    refs https://github.com/w3c/csswg-drafts/issues/8835

--- mod.js
+++ mod.js
@@ -52,9 +52,23 @@ export const types = {
 };
 const hexDigit = (cur) => (cur ^ 0x30) <= 9 ||
     ((cur | 0x20) >= 0x0061 && (cur | 0x20) <= 0x0066);
-const identStart = (cur) => cur === 0x005f ||
-    cur >= 0x0080 ||
-    ((cur | 0x20) >= 0x0061 && (cur | 0x20) <= 0x007a);
+const identStart = (cur) => ((cur | 0x20) >= 0x0061 && (cur | 0x20) <= 0x007a) ||
+    cur === 0x005f ||
+    cur === 0x200c ||
+    cur === 0x200d ||
+    cur === 0x203f ||
+    cur === 0x2040 ||
+    cur === 0x00b7 ||
+    (cur >= 0x00c0 && cur <= 0x00d6) ||
+    (cur >= 0x00d8 && cur <= 0x00f6) ||
+    (cur >= 0x00f8 && cur <= 0x037d) ||
+    (cur >= 0x037f && cur <= 0x1fff) ||
+    (cur >= 0x2070 && cur <= 0x218f) ||
+    (cur >= 0x2c00 && cur <= 0x2fef) ||
+    (cur >= 0x3001 && cur <= 0xd7ff) ||
+    (cur >= 0xf900 && cur <= 0xfdcf) ||
+    (cur >= 0xfdf0 && cur <= 0xfffd) ||
+    cur >= 0x10000;
 const ident = (cur) => cur === 0x002d || (cur ^ 0x30) <= 9 || identStart(cur);
 const newline = (cur) => cur === 0x000a || cur === 0x000c || cur === 0x000d;
 const whitespace = (cur) => newline(cur) || cur === 0x0020 || cur === 0x0009;
@@ -93,30 +107,31 @@ for (let i = 0; i < 128; i += 1) {
             (identStart(i) && IDENTSTART_RANGE) ||
             i;
 }
-const charCodeRange = (str, pos) => {
-    return ranges[str.charCodeAt(pos)] || IDENTSTART_RANGE;
+const codePointRange = (str, pos) => {
+    const i = str.codePointAt(pos);
+    return ranges[i] || i;
 };
-const escapeStartSequence = (str, pos) => str.charCodeAt(pos) === 0x005c && !newline(str.charCodeAt(pos + 1));
+const escapeStartSequence = (str, pos) => str.codePointAt(pos) === 0x005c && !newline(str.codePointAt(pos + 1));
 const numberStartSequence = (str, pos) => {
-    const firstRange = charCodeRange(str, pos);
-    const secondRange = charCodeRange(str, pos + 1);
+    const firstRange = codePointRange(str, pos);
+    const secondRange = codePointRange(str, pos + 1);
     return (firstRange === DIGIT_RANGE ||
         (firstRange === SIGN_RANGE && secondRange === DIGIT_RANGE) ||
         (firstRange === SIGN_RANGE &&
             secondRange === 0x002e &&
-            charCodeRange(str, pos + 2) === DIGIT_RANGE) ||
+            codePointRange(str, pos + 2) === DIGIT_RANGE) ||
         (firstRange === 0x002e && secondRange === DIGIT_RANGE));
 };
 const identStartSequence = (str, pos) => {
-    if (str.charCodeAt(pos) === 0x002d)
+    if (str.codePointAt(pos) === 0x002d)
         pos += 1;
-    if (str.charCodeAt(pos) === 0x002d)
+    const char = str.codePointAt(pos);
+    if (char === 0x002d)
         return true;
-    return ((str.length > pos && charCodeRange(str, pos) === IDENTSTART_RANGE) ||
-        escapeStartSequence(str, pos));
+    return identStart(char) || escapeStartSequence(str, pos);
 };
 const consumeRangeSequence = (str, pos, range) => {
-    while (charCodeRange(str, pos) === range)
+    while (codePointRange(str, pos) === range)
         pos += 1;
     return pos;
 };
@@ -124,16 +139,16 @@ const consumeEscapeSequence = (str, pos) => {
     pos += 2;
     if (pos > str.length)
         return str.length;
-    if (hexDigit(str.charCodeAt(pos - 1))) {
-        for (let i = 0; hexDigit(str.charCodeAt(pos)) && i < 5; i += 1, pos += 1) { }
-        if (charCodeRange(str, pos) === WHITESPACE_RANGE)
+    if (hexDigit(str.codePointAt(pos - 1))) {
+        for (let i = 0; hexDigit(str.codePointAt(pos)) && i < 5; i += 1, pos += 1) { }
+        if (codePointRange(str, pos) === WHITESPACE_RANGE)
             pos += 1;
     }
     return pos;
 };
 const REPLACEMENT = "\ufffd";
 const decodeEscapeSequence = (str) => {
-    if (!hexDigit(str.charCodeAt(0))) {
+    if (!hexDigit(str.codePointAt(0))) {
         return str[0] || REPLACEMENT;
     }
     const codePoint = parseInt(str, 16) || 0;
@@ -147,8 +162,9 @@ const decodeEscapeSequence = (str) => {
 const consumeIdentSequence = (str, pos) => {
     const len = str.length;
     while (pos < len) {
-        if (ident(str.charCodeAt(pos))) {
-            pos += 1;
+        const codepoint = str.codePointAt(pos);
+        if (ident(codepoint)) {
+            pos += 1 + +(codepoint > 0xffff);
         }
         else if (escapeStartSequence(str, pos)) {
             pos = consumeEscapeSequence(str, pos);
@@ -162,9 +178,10 @@ const consumeIdentSequence = (str, pos) => {
 const decodeIdentSequence = (str) => {
     let identValue = "";
     for (let pos = 0; pos < str.length;) {
-        if (ident(str.charCodeAt(pos))) {
-            identValue += str[pos];
-            pos += 1;
+        const codepoint = str.codePointAt(pos);
+        if (ident(codepoint)) {
+            identValue += String.fromCodePoint(codepoint);
+            pos += 1 + +(codepoint > 0xffff);
         }
         else if (escapeStartSequence(str, pos)) {
             identValue += decodeEscapeSequence(str.slice(pos + 1, (pos = consumeEscapeSequence(str, pos))));
@@ -176,16 +193,16 @@ const decodeIdentSequence = (str) => {
     return identValue;
 };
 const consumeNumberSequence = (str, pos) => {
-    let char = charCodeRange(str, pos);
+    let char = codePointRange(str, pos);
     if (char === SIGN_RANGE || char === 0x002e)
         pos += 1;
     pos = consumeRangeSequence(str, pos, DIGIT_RANGE);
-    if (charCodeRange(str, pos) === 0x002e &&
-        charCodeRange(str, pos + 1) === DIGIT_RANGE) {
+    if (codePointRange(str, pos) === 0x002e &&
+        codePointRange(str, pos + 1) === DIGIT_RANGE) {
         pos = consumeRangeSequence(str, pos + 2, DIGIT_RANGE);
     }
-    char = charCodeRange(str, pos + 1);
-    if ((str.charCodeAt(pos) | 32) === 0x0065 &&
+    char = codePointRange(str, pos + 1);
+    if ((str.codePointAt(pos) | 32) === 0x0065 &&
         (char === SIGN_RANGE || char === DIGIT_RANGE)) {
         pos = consumeRangeSequence(str, pos + 2, DIGIT_RANGE);
     }
@@ -196,8 +213,8 @@ const consumeURLSequence = (str, pos) => {
     const len = str.length;
     pos = consumeRangeSequence(str, pos, WHITESPACE_RANGE);
     while (pos < len) {
-        let char = str.charCodeAt(pos);
-        let charRange = charCodeRange(str, pos);
+        let char = str.codePointAt(pos);
+        let charRange = codePointRange(str, pos);
         if (char === 0x029) {
             pos += 1;
             break;
@@ -219,7 +236,7 @@ const consumeURLSequence = (str, pos) => {
         }
         else if (charRange === WHITESPACE_RANGE) {
             pos = consumeRangeSequence(str, pos, WHITESPACE_RANGE);
-            char = str.charCodeAt(pos);
+            char = str.codePointAt(pos);
             if (!char) {
                 break;
             }
@@ -240,12 +257,12 @@ const consumeURLSequence = (str, pos) => {
 const consumeIdentLikeToken = (str, pos) => {
     const lastPos = pos;
     pos = consumeIdentSequence(str, pos);
-    if (str.charCodeAt(pos) === 0x0028) {
+    if (str.codePointAt(pos) === 0x0028) {
         const whitePos = consumeRangeSequence(str, pos + 1, WHITESPACE_RANGE);
         pos += 1;
         if (whitePos - pos <= 4 &&
             decodeIdentSequence(str.slice(lastPos, pos - 1)).toLowerCase() == "url" &&
-            charCodeRange(str, whitePos) !== QUOTE_RANGE) {
+            codePointRange(str, whitePos) !== QUOTE_RANGE) {
             return consumeURLSequence(str, pos);
         }
         return [FUNCTION, pos];
@@ -255,7 +272,7 @@ const consumeIdentLikeToken = (str, pos) => {
 const consumeNumericToken = (str, pos) => {
     pos = consumeNumberSequence(str, pos);
     let token = NUMBER;
-    if (str.charCodeAt(pos) === 0x025) {
+    if (str.codePointAt(pos) === 0x025) {
         pos += 1;
         token = PERCENTAGE;
     }
@@ -266,11 +283,11 @@ const consumeNumericToken = (str, pos) => {
     return [token, pos];
 };
 const consumeStringToken = (str, pos) => {
-    const quote = str.charCodeAt(pos);
+    const quote = str.codePointAt(pos);
     pos += 1;
     const len = str.length;
     while (pos < len) {
-        switch (str.charCodeAt(pos)) {
+        switch (str.codePointAt(pos)) {
             case quote:
                 pos += 1;
                 return [STRING, pos];
@@ -282,7 +299,7 @@ const consumeStringToken = (str, pos) => {
                 if (pos == len - 1) {
                     pos += 1;
                 }
-                else if (newline(str.charCodeAt(pos + 1))) {
+                else if (newline(str.codePointAt(pos + 1))) {
                     pos += 2;
                 }
                 else if (escapeStartSequence(str, pos)) {
@@ -301,7 +318,7 @@ export function* lex(str) {
     let start = 0;
     let pos = 0;
     while (pos < len) {
-        switch (charCodeRange(str, pos)) {
+        switch (codePointRange(str, pos)) {
             case WHITESPACE_RANGE:
                 pos = consumeRangeSequence(str, pos, WHITESPACE_RANGE);
                 token = WHITESPACE;
@@ -316,13 +333,13 @@ export function* lex(str) {
                 if (numberStartSequence(str, pos)) {
                     [token, pos] = consumeNumericToken(str, pos);
                 }
-                else if (str.charCodeAt(pos) === 0x002d &&
-                    str.charCodeAt(pos + 1) === 0x002d &&
-                    str.charCodeAt(pos + 2) === 0x003e) {
+                else if (str.codePointAt(pos) === 0x002d &&
+                    str.codePointAt(pos + 1) === 0x002d &&
+                    str.codePointAt(pos + 2) === 0x003e) {
                     token = CDC;
                     pos += 3;
                 }
-                else if (str.charCodeAt(pos) === 0x002d &&
+                else if (str.codePointAt(pos) === 0x002d &&
                     identStartSequence(str, pos)) {
                     [token, pos] = consumeIdentLikeToken(str, pos);
                 }
@@ -344,9 +361,9 @@ export function* lex(str) {
                 [token, pos] = consumeIdentLikeToken(str, pos);
                 break;
             case 0x003c:
-                if (str.charCodeAt(pos + 1) === 0x0021 &&
-                    str.charCodeAt(pos + 2) === 0x002d &&
-                    str.charCodeAt(pos + 3) === 0x002d) {
+                if (str.codePointAt(pos + 1) === 0x0021 &&
+                    str.codePointAt(pos + 2) === 0x002d &&
+                    str.codePointAt(pos + 3) === 0x002d) {
                     token = CDO;
                     pos += 4;
                 }
@@ -356,7 +373,7 @@ export function* lex(str) {
                 }
                 break;
             case 0x0023:
-                if (ident(str.charCodeAt(pos + 1)) ||
+                if (ident(str.codePointAt(pos + 1)) ||
                     escapeStartSequence(str, pos + 1)) {
                     pos = consumeIdentSequence(str, pos + 1);
                     token = HASH;
@@ -386,7 +403,7 @@ export function* lex(str) {
                 }
                 break;
             case 0x002f:
-                if (str.charCodeAt(pos + 1) === 0x002a) {
+                if (str.codePointAt(pos + 1) === 0x002a) {
                     pos = str.indexOf("*/", pos + 2);
                     if (pos === -1)
                         pos = len;
@@ -400,12 +417,17 @@ export function* lex(str) {
                 break;
             case SPECIAL_DELIM_RANGE:
                 token =
-                    specialDelims[str.charCodeAt(pos)];
+                    specialDelims[str.codePointAt(pos)];
                 pos += 1;
                 break;
             default:
-                token = DELIM;
-                pos += 1;
+                if (identStartSequence(str, pos)) {
+                    [token, pos] = consumeIdentLikeToken(str, pos);
+                }
+                else {
+                    token = DELIM;
+                    pos += 1;
+                }
         }
         yield [token, start, (start = pos)];
     }
@@ -415,17 +437,17 @@ export const value = (str, [token, start, end]) => {
         case COMMENT:
             return str.slice(start + 2, end - 2);
         case STRING: {
-            const quote = str.charCodeAt(start);
+            const quote = str.codePointAt(start);
             let value = "";
             for (let pos = start + 1; pos < end; pos += 1) {
-                switch (str.charCodeAt(pos)) {
+                switch (str.codePointAt(pos)) {
                     case quote:
                         return value;
                     case 0x005c:
                         if (pos + 1 === end && end === str.length) {
                             return value;
                         }
-                        else if (newline(str.charCodeAt(pos + 1))) {
+                        else if (newline(str.codePointAt(pos + 1))) {
                             pos += 1;
                             break;
                         }
@@ -480,9 +502,9 @@ export const value = (str, [token, start, end]) => {
                     pos += 1;
                     break;
                 }
-                else if (quote(str.charCodeAt(pos)) ||
+                else if (quote(str.codePointAt(pos)) ||
                     char === "(" ||
-                    nonPrintable(str.charCodeAt(pos))) {
+                    nonPrintable(str.codePointAt(pos))) {
                     return null;
                 }
                 else if (char === "\\") {
@@ -493,7 +515,7 @@ export const value = (str, [token, start, end]) => {
                         return null;
                     }
                 }
-                else if (whitespace(str.charCodeAt(pos))) {
+                else if (whitespace(str.codePointAt(pos))) {
                     pos = consumeRangeSequence(str, pos, WHITESPACE_RANGE);
                     char = str[pos];
                     if (!char) {
